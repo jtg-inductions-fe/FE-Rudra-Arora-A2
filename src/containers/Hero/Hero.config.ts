435686b94ee83e10ddcb3cd77af45de4fe6328d3
rudra arora <rudra.arora@joshtechnologygroup.com>
2025-10-29 13:02:53 +0530

    [RA-A2-improvements]: Improvements

--- src/containers/Hero/Hero.config.ts
+++ src/containers/Hero/Hero.config.ts
@@ -1,25 +1,20 @@
-import hero_image_1 from '@assets/images/hero-image-1.webp';
-import hero_image_2 from '@assets/images/hero-image-2.webp';
-import hero_image_3 from '@assets/images/hero-image-3.webp';
-import hero_image_4 from '@assets/images/hero-image-4.webp';
-
 export const heroConfig = [
     {
-        img: hero_image_1,
+        img: 'https://res.cloudinary.com/dmc0thrp7/image/upload/v1761722098/hero-image-1_bzr49p.webp',
         title: 'hero-Image-1',
         rows_sm: 2,
         cols_sm: 4,
         rows_lg: 2,
     },
     {
-        img: hero_image_2,
+        img: 'https://res.cloudinary.com/dmc0thrp7/image/upload/v1761722097/hero-image-2_usqzl6.webp',
         title: 'hero-Image-2',
         cols_sm: 4,
         rows_sm: 1,
         rows_lg: 2,
     },
     {
-        img: hero_image_3,
+        img: 'https://res.cloudinary.com/dmc0thrp7/image/upload/v1761722098/hero-image-3_qx85gb.webp',
         title: 'hero-Image-3',
         rows_lg: 3,
         cols_lg: 2,
@@ -27,7 +22,7 @@ export const heroConfig = [
         rows_sm: 1,
     },
     {
-        img: hero_image_4,
+        img: 'https://res.cloudinary.com/dmc0thrp7/image/upload/v1761722097/hero-image-4_jp4kv2.webp',
         title: 'hero-Image-4',
         cols_lg: 2,
         rows_lg: 1,
